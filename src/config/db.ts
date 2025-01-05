93ab2c3da27a2a7eb0ef22f98faabd462ec6e75f
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-01-05 04:07:03 -0400

    add script & change path in config/db to upload serve to production

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-  models: [__dirname + "/../models/**/*.ts"], //path of models
+  models: [__dirname + "/../models/**/*"], //path of models
 });
 
 export default db;
